dc16ec7715538c951a7069faaa8c36bc66aaa76d
Sergejs Luhmirins <sergejs@fullcontact.com>
2017-10-26 23:13:34 +0300

    Part 4 - Even more Kotlin

--- code/src/main/kotlin/Index.kt
+++ code/src/main/kotlin/Index.kt
@@ -1,8 +1,15 @@
-external fun require(module: String): dynamic
+import bindings.Admin
+import bindings.Date
+import bindings.Functions
+
 external val exports: dynamic
 
-val functions = require("firebase-functions")
-val admin = require("firebase-admin")
+data class Rates(
+    val usd: Float,
+    val czk: Float,
+    val pln: Float,
+    val rub: Float
+)
 
 data class Conversions(
     val eur: Float,
@@ -14,11 +21,34 @@ data class Conversions(
 )
 
 fun main(args: Array<String>) {
-    admin.initializeApp(functions.config().firebase)
+    Admin.initializeApp(Functions.config().firebase)
 
-    exports.testPush = functions.https.onRequest { req, res ->
-        admin.database().ref("/conversions")
+    exports.testPush = Functions.https.onRequest { req, res ->
+        Admin.database().ref("/conversions")
             .push(Conversions(req.query.eur.toString().toFloat()))
             .then { res.status(200).send("done") }
+    }
+
+    exports.convert = Functions.database.ref("/conversions/{conversionID}").onWrite<Conversions> { event ->
+        val input = event.data.`val`()
+        if (input.timestamp != null) return@onWrite
+        Admin.database().ref("/rates").once<Rates>("value")
+            .then<Conversions> { ratesSnap ->
+                ratesSnap.`val`().let {
+                    Conversions(
+                        eur = input.eur,
+                        usd = input.eur * it.usd,
+                        czk = input.eur * it.czk,
+                        pln = input.eur * it.pln,
+                        rub = input.eur * it.rub,
+                        timestamp = Date.now()
+                    )
+                }
+            }
+            .then { result ->
+                Admin.database().ref("/conversions")
+                    .child(event.params.conversionID)
+                    .set(result)
+            }
     }
 }
